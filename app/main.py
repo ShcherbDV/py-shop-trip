8647abc00ba9943b1c19cd9f5970b9f9ed292b1b
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-07-17 22:59:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.shop import Shop
 
 
 def shop_trip() -> None:
-    with open("app\\config.json") as config_file:
+    with open("config.json") as config_file:
         config_data = json.load(config_file)
 
     fuel_price = config_data["FUEL_PRICE"]
